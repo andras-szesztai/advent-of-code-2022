a9bf44c686964e97e304e3cb4243636da5fe1353
András Szesztai <andras.szesztai@nielsen.com>
2022-12-10 11:33:14 +0100

    2022 day 10 part one solution

---
+++ src/2022/day10/day10.test.ts
@@ -0,0 +1,108 @@
+import fs from 'fs'
+
+import {
+    getPerCycleSignalStrength,
+    getSignalStrengthsForSpecificCycles,
+    getSignalStrengthSum,
+    parseInstruction,
+    ProgramStep,
+} from './day10'
+
+const testProgramOne = fs
+    .readFileSync(
+        '/Users/anszeszt2101/GitHub/advent-of-code/src/2022/day10/programOne.txt',
+        'utf8'
+    )
+    .split('\n')
+    .map((d) => {
+        const instructionElements = d.split(' ')
+        return [
+            instructionElements[0],
+            instructionElements[1] ? +instructionElements[1] : 0,
+        ]
+    }) as ProgramStep[]
+
+const testProgramTwo = fs
+    .readFileSync(
+        '/Users/anszeszt2101/GitHub/advent-of-code/src/2022/day10/programTwo.txt',
+        'utf8'
+    )
+    .split('\n')
+    .map((d) => {
+        const instructionElements = d.split(' ')
+        return [
+            instructionElements[0],
+            instructionElements[1] ? +instructionElements[1] : 0,
+        ]
+    }) as ProgramStep[]
+
+describe('day10', () => {
+    describe('parseInstruction', () => {
+        it('should return the correctly parsed instruction', () => {
+            expect(parseInstruction('addx', 3)).toEqual([0, 3])
+            expect(parseInstruction('addx', -2)).toEqual([0, -2])
+            expect(parseInstruction('noop', 0)).toEqual([0])
+        })
+    })
+
+    describe('getPerCycleSignalStrength', () => {
+        it('returns correct signal strength per each cycle', () => {
+            const smallTestProgram = [
+                ['noop', 0],
+                ['addx', 3],
+                ['addx', -5],
+            ] as ProgramStep[]
+            expect(getPerCycleSignalStrength(smallTestProgram)).toEqual(
+                new Map([
+                    [1, 1],
+                    [2, 1],
+                    [3, 1],
+                    [4, 4],
+                    [5, 4],
+                    [6, -1],
+                ])
+            )
+            const result = getPerCycleSignalStrength(testProgramOne)
+            expect(result.get(20)).toBe(21)
+            expect(result.get(60)).toBe(19)
+            expect(result.get(100)).toBe(18)
+            expect(result.get(140)).toBe(21)
+            expect(result.get(180)).toBe(16)
+            expect(result.get(220)).toBe(18)
+        })
+    })
+
+    describe('getSignalStrengthsForSpecificCycles', () => {
+        it('returns correct signal strength array for specific cycles', () => {
+            const result = getSignalStrengthsForSpecificCycles(
+                testProgramOne,
+                [20, 60, 100, 140, 180, 220]
+            )
+            expect(result).toEqual([420, 1140, 1800, 2940, 2880, 3960])
+        })
+        it('returns signal strength only for existing cycles', () => {
+            const result = getSignalStrengthsForSpecificCycles(
+                testProgramOne,
+                [20, 60, 400]
+            )
+            expect(result).toEqual([420, 1140])
+        })
+    })
+
+    describe('getSignalStrengthSum', () => {
+        it('returns correct signal strength sum for specific cycles', () => {
+            const result = getSignalStrengthSum(
+                testProgramOne,
+                [20, 60, 100, 140, 180, 220]
+            )
+            expect(result).toEqual(13_140)
+        })
+        it('returns part one solution', () => {
+            const result = getSignalStrengthSum(
+                testProgramTwo,
+                [20, 60, 100, 140, 180, 220]
+            )
+            expect(result).toEqual(12_740)
+        })
+    })
+})
---
+++ src/2022/day10/day10.ts
@@ -0,0 +1,56 @@
+type Instruction = 'addx' | 'noop'
+export type ProgramStep = [instruction: Instruction, valueChange: number]
+
+export const parseInstruction = (
+    instruction: Instruction,
+    valueChange: number
+) => {
+    const parsedInstructions = [0]
+    if (instruction === 'addx') {
+        parsedInstructions.push(valueChange)
+    }
+    return parsedInstructions
+}
+
+export const getPerCycleSignalStrength = (program: ProgramStep[]) => {
+    const perCycleSignalStrength = new Map<number, number>([[1, 1]])
+    let currentCycle = 0
+    program.forEach(([instruction, valueChange]) => {
+        const cycles = parseInstruction(instruction, valueChange)
+        cycles.forEach((change) => {
+            currentCycle++
+            perCycleSignalStrength.set(
+                currentCycle + 1,
+                perCycleSignalStrength.get(currentCycle)! + change
+            )
+        })
+    })
+    return perCycleSignalStrength
+}
+
+export const getSignalStrengthsForSpecificCycles = (
+    program: ProgramStep[],
+    cycles: number[]
+) => {
+    const perCycleSignalStrength = getPerCycleSignalStrength(program)
+    return cycles
+        .map((cycle) => {
+            const signalStrength = perCycleSignalStrength.get(cycle)
+            return signalStrength ? cycle * signalStrength : null
+        })
+        .filter((signalStrength) => signalStrength !== null) as number[]
+}
+
+export const getSignalStrengthSum = (
+    program: ProgramStep[],
+    cycles: number[]
+) => {
+    const signalStrengthForSpecificCycles = getSignalStrengthsForSpecificCycles(
+        program,
+        cycles
+    )
+    return signalStrengthForSpecificCycles.reduce(
+        (sum, signalStrength) => sum + signalStrength,
+        0
+    )
+}
